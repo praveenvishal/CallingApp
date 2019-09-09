2a3c3cce381661c7e3dc21c4d42b7bae9ec4ec92
Nitish <nitish.chaudhary@techaheadcorp.com>
2019-09-09 18:29:01 +0530

    Firebase

--- app/src/main/java/com/appocean/callingapp/util/Util.java
+++ app/src/main/java/com/appocean/callingapp/util/Util.java
@@ -3,8 +3,16 @@ package com.appocean.callingapp.util;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.appocean.callingapp.ApplicationController;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.google.common.eventbus.EventBus;
+
+import org.json.JSONObject;
 
 public class Util {
     public static int dpToPx(int dp) {
@@ -24,5 +32,30 @@ public class Util {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static void getFbDetails(AccessToken accessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            Log.e("FbResponse", object.toString());
+                         //   SocialMediaInfoBean socialMediaInfoBean = fromJson(object.toString(), SocialMediaInfoBean.class);
+
+                            //Send result back to login screen
+                          //  EventBus.getDefault().post(socialMediaInfoBean);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+        Bundle parameters = new Bundle();
+        //parameters.putString("fields", "id,name,gender,birthday,friends,email,education,first_name,last_name,location,relationship_status,work,about,posts.limit(3){id,picture},photos.limit(6){id,picture},picture,albums,likes.limit(100)");
+        parameters.putString("fields", "id,name,email,picture");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
